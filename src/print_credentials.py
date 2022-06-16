e3274b799d4e6a916be4715c2d464bf607041cb0
paper2parasol <paper2parasol@gmail.com>
2022-06-16 21:18:02 +0900

    fix photo libraryのscopeを修正

--- src/print_credentials.py
+++ src/print_credentials.py
@@ -1,10 +1,10 @@
 import google_auth_oauthlib
 
 CLIENT_SECRET_FILE = 'client_secret.json'
-SCOPES = ['https://www.googleapis.com/auth/photoslibrary']
+SCOPES = ['https://www.googleapis.com/auth/photoslibrary.readonly']
 
 flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        CLIENT_SECRET_FILE, scopes=SCOPES)
+    CLIENT_SECRET_FILE, scopes=SCOPES)
 credentials = flow.run_console()
 
 print('CREDENTIALS_JSON: ' + credentials.to_json())
