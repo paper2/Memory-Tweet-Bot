5e7bdb642c4cf78a590e5b8a1d0f72c20772e2c9
paper2parasol <paper2parasol@gmail.com>
2022-06-18 11:32:07 +0900

    add cloud loggingを統合

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import functions_framework
 import google_photo
 import twitter
+import google.cloud.logging
 
 PROJECT_ID = 'rising-beach-352202'
 GOOGLE_OAUTH_CREDENITIALS_SECRET_ID = 'google-oauth-credentials'
@@ -9,6 +10,12 @@ TWITTER_CREDENITIALS_SECRET_ID = 'twitter-credentials'
 
 @functions_framework.cloud_event
 def memory_tweet(cloud_event):
+    # Cloud Loggingと統合。エラー発生箇所なども記録されるので便利。
+    # ログはCloud Loggingで確認する。
+    client = google.cloud.logging.Client()
+    # デフォルトでINFO以上が収集される。
+    client.setup_logging()
+
     # Google Photoから今日の写真をランダムで取得。
     google_oauth_credentials = google_photo.getCredentialsFromSecretManager(
         PROJECT_ID, GOOGLE_OAUTH_CREDENITIALS_SECRET_ID, 'latest')
