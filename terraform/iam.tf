de6afc8361d6f8e2bc5e9fd2d6333f59f0c426d6
paper2parasol <paper2parasol@gmail.com>
2022-06-18 09:13:52 +0900

    add SAにSecret Versionの追加権限を付与

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -3,8 +3,18 @@ resource "google_secret_manager_secret_iam_member" "memory_tweet_function_google
   role      = "roles/secretmanager.secretAccessor"
   member    = "serviceAccount:${google_service_account.memory_tweet_function.email}"
 }
+resource "google_secret_manager_secret_iam_member" "memory_tweet_function_google_oauth_credentials_secretversionadder" {
+  secret_id = google_secret_manager_secret.google_oauth_credentials.id
+  role      = "roles/secretmanager.secretVersionAdder"
+  member    = "serviceAccount:${google_service_account.memory_tweet_function.email}"
+}
 resource "google_secret_manager_secret_iam_member" "memory_tweet_function_twitter_credentials_secretaccessor" {
   secret_id = google_secret_manager_secret.twitter_credentials.id
   role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_service_account.memory_tweet_function.email}"
+}
+resource "google_secret_manager_secret_iam_member" "memory_tweet_function_twitter_credentials_secretversionadder" {
+  secret_id = google_secret_manager_secret.twitter_credentials.id
+  role      = "roles/secretmanager.secretVersionAdder"
   member    = "serviceAccount:${google_service_account.memory_tweet_function.email}"
 }
